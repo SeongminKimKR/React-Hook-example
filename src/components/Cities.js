f411d1c7b9eaa996284a9e96bdbf74f44e10b6aa
SeongminKimKR <67994481+SeongminKimKR@users.noreply.github.com>
2020-07-25 10:35:17 +0900

    Rename weather.js to index.js

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, withRouter } from "react-router";
 import CityList from "./CityList";
-import Weather from "./Weather/weathers";
+import Weather from "./Weather";
 
 const API_CITIES = "http://localhost:8888/weather-service/available-cities";
 
